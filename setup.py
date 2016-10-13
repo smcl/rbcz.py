107c265d01e96e61ca928612b8b6605aac4aea23
smcl <sean.mclemon@gmail.com>
2016-10-13 16:26:08 +0200

    bumped version number for 0.3 rel, forgot to include a file. what a pro

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'rbcz',
   packages = ['rbcz'],
-  version = '0.2',
+  version = '0.3',
   description = 'library for interacting with Czech Raiffeisen Bank\'s text bank statements',
   author = 'Sean McLemon',
   author_email = 'sean.mclemon@gmail.com',
   url = 'https://github.com/smcl/rbcz.py',
-  download_url = 'https://github.com/smcl/rbcz.py/tarball/0.2',
+  download_url = 'https://github.com/smcl/rbcz.py/tarball/0.3',
   keywords = ['banking', 'raiffeisen', 'czech', 'cz'],
   classifiers = [],
 )
