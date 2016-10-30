2bcdea1e12d3800d4b9b3e1d3523dec43bf3026b
smcl <sean.mclemon@gmail.com>
2016-10-30 17:53:53 +0100

    pypi docs

--- setup.py
+++ setup.py
@@ -1,14 +1,23 @@
 from setuptools import setup
 
+current_version = '0.6'
+
+# convert from github markdown to rst
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except(IOError, ImportError):
+    long_description = open('README.md').read()
+
 setup(
     name = 'rbcz',
     packages = [ 'rbcz', 'rbcz.test' ],
-    version = '0.5',
+    version = current_version,
     description = 'library for interacting with Czech Raiffeisen Bank\'s text bank statements',
     author = 'Sean McLemon',
     author_email = 'sean.mclemon@gmail.com',
     url = 'https://github.com/smcl/rbcz.py',
-    download_url = 'https://github.com/smcl/rbcz.py/tarball/0.5',
+    download_url = 'https://github.com/smcl/rbcz.py/tarball/%s' % (current_version),
     keywords = ['banking', 'raiffeisen', 'czech', 'cz'],
     classifiers = [],
     test_suite='rbcz.test.all',
@@ -18,5 +27,5 @@ setup(
     setup_requires=[
         'unittest2'
     ],
-    
+    long_description=long_description
 )
